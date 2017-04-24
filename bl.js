04acff0c01718e82c70b89c3a29fdb55e66a4cdc
teak1 <trainian@gmail.com>
2017-04-24 13:38:26 -0700

    Update bl.js

--- bl.js
+++ bl.js
@@ -1,2 +1,14 @@
-///test
-alert("test");
+var keywords = ["minecraft","video game","card smith","cardsmith","code","flight"];
+var inf = {};
+var clr = false;
+for(var i = 0;i<keywords.length;i++){
+  if(document.body.innerHTML.split(keywords[i]).length>1){
+    inf[keywords[i]]=true;
+    clr=true;
+  }else{
+    inf[keywords[i]]=false;
+  }
+}
+if(clr){
+  alert(inf);
+}
