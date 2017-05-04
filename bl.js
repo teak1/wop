0aea3df3a836ed8aea0033c99ef1437965d6029c
teak1 <trainian@gmail.com>
2017-05-04 14:45:53 -0700

    Update bl.js

--- bl.js
+++ bl.js
@@ -1,38 +1,42 @@
 var letters = "xu4?!4welm($$";
-function exec(){
-var cls = true;
-var keywords = ["minecraft","video game","card smith","cardsmith","learn code","flight","game","magic","larp"];
-var excludes = ["https://www.google.com","https://github.com","https://kahoot.it","https://mail.google.com","https://quizizz.com","https://quizlet.com","https://youtube.com"];
-var inf = {};
-var clr = false;
-for(var i = 0;i<keywords.length;i++){
-  if(document.body.innerHTML.split(keywords[i]).length>1 && !excludes.includes(window.location.origin)){
-    inf[keywords[i]]=true;
-    clr=true;
-  }else{
-    inf[keywords[i]]=false;
-  }
+
+function exec() {
+    var cls = true;
+    var keywords = ["minecraft", "video game", "card smith", "cardsmith", "learn code", "flight", "game", "magic", "larp"];
+    var excludes = ["https://www.google.com", "https://github.com", "https://kahoot.it", "https://mail.google.com", "https://quizizz.com", "https://quizlet.com", "https://youtube.com"];
+    var inf = {};
+    var clr = false;
+    for (var i = 0; i < keywords.length; i++) {
+        if (document.body.innerHTML.split(keywords[i]).length > 1 && !excludes.includes(window.location.origin)) {
+            inf[keywords[i]] = true;
+            cls=true;
+            clr = true;
+        } else {
+            inf[keywords[i]] = false;
+        }
+    }
+    if (clr) {
+        if (confirm("override redirect?")) {
+            var pswd = prompt("password");
+            if (pswd === getPassword()) {
+                clr = false;
+              	cls = false;
+                exec = new Function("");
+            }
+        }
+        console.log(JSON.stringify(inf));
+        //alert(JSON.stringify(inf));
+        if (cls) window.location = "https://www.google.com";
+    }
 }
-if(clr){
-  if(confirm("override redirect?")){
-  var pswd = prompt("password");
-  if(pswd===getPassword()){
-    clr = false;
-    exec = new Function("");
-  }
-  }
-  console.log(JSON.stringify(inf));
-  //alert(JSON.stringify(inf));
-  if(cls)window.location="https://www.google.com";
-}
-}
-setInterval(exec,1000);
-function getPassword(){
-  var o = "";
-  var b = 0;
-  for(var i =0;i<letters.length;i++){
-    b = letters.charCodeAt(i);
-    o+=String.fromCodePoint(b+=letters.charCodeAt(i));
-  }
-  return o;
+setTimeout(exec, 1000);
+
+function getPassword() {
+    var o = "";
+    var b = 0;
+    for (var i = 0; i < letters.length; i++) {
+        b = letters.charCodeAt(i);
+        o += String.fromCodePoint(b += letters.charCodeAt(i));
+    }
+    return o;
 }
