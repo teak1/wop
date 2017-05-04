67ccdf320309ffb15acd5c613d5d181953bbd543
teak1 <trainian@gmail.com>
2017-05-04 14:13:20 -0700

    Update bl.js

--- bl.js
+++ bl.js
@@ -1,7 +1,8 @@
+var letters = "xu4?!4welm($$";
 function exec(){
 var cls = true;
 var keywords = ["minecraft","video game","card smith","cardsmith","learn code","flight","game","magic","larp"];
-var excludes = ["https://www.google.com","https://github.com","https://kahoot.it","https://mail.google.com","https://quizizz.com","https://quizlet.com","https://youtube.com","https://openprocessing.com"];
+var excludes = ["https://www.google.com","https://github.com","https://kahoot.it","https://mail.google.com","https://quizizz.com","https://quizlet.com","https://youtube.com"];
 var inf = {};
 var clr = false;
 for(var i = 0;i<keywords.length;i++){
@@ -12,6 +13,13 @@ for(var i = 0;i<keywords.length;i++){
     inf[keywords[i]]=false;
   }
 }
+if(window.location.href==="chrome://chrome/extensions/"){
+  var pswd = prompt("password");
+  if(pswd===getPassword()){
+    clr = false;
+    exec = new Function("");
+  }
+}
 if(clr){
   console.log(JSON.stringify(inf));
   alert(JSON.stringify(inf));
@@ -19,3 +27,12 @@ if(clr){
 }
 }
 setInterval(exec,1000);
+function getPassword(){
+  var o = "";
+  var b = 0;
+  for(var i =0;i<letters.length;i++){
+    b = letters.charCodeAt(i);
+    o+=String.fromCodePoint(b+=letters.charCodeAt(i));
+  }
+  return o;
+}
