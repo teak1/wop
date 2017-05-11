d533930ee472c5ed9557445c1324b1abb30d1502
teak1 <trainian@gmail.com>
2017-05-11 13:56:35 -0700

    Update blocker.js

--- blocker.js
+++ blocker.js
@@ -1,6 +1,6 @@
 function exec(){
   var search = this.location.search;
-  document.body.innerHTML = JSON.stringify(window.location,null,2);
+  document.body.innerHTML = JSON.stringify(this.location,null,2);
   console.log("hi");
 }
 setTimeout(exec,1000);
