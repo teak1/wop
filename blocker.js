7c9f41bcaa5799591140fc7db32ffb3ef73abdc1
teak1 <trainian@gmail.com>
2017-05-11 13:52:33 -0700

    Update blocker.js

--- blocker.js
+++ blocker.js
@@ -1,6 +1,6 @@
 function exec(){
   var search = window.location.search;
-  document.body.innerHTML = JSSON.stringify(window.location,null,2);
+  document.body.innerHTML = JSON.stringify(window.location,null,2);
   alert("hi");
 }
 setTimeout(exec,1000);
