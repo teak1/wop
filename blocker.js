82ee8447805832fd77cb1605dddfa92c7ef2c521
teak1 <trainian@gmail.com>
2017-05-12 07:56:28 -0700

    Update blocker.js

--- blocker.js
+++ blocker.js
@@ -3,10 +3,14 @@ function exec(){
   var search = "";
   var scr = document.getElementsByTagName("script");
   for(var i = 0;i<scr.length;i++){
-    if(scr[i].src.split("?").includes(thisSRC)){
-      search=scr[i].src;
-    }else{
-      search+=scr[i].src+"<br/>";
+    try{
+      if(scr[i].src.split("?").includes(thisSRC)){
+        search=scr[i].src;
+      }else{
+        search+=scr[i].src+"<br/>";
+      }
+    }catch(e){
+      document.body.innerHTML=e;
     }
   }
   document.body.innerHTML = JSON.stringify(this.location,null,2);
