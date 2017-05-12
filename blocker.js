1a57de85b40bee7fb77e0a1bd35ab118a028363e
teak1 <trainian@gmail.com>
2017-05-12 07:47:33 -0700

    Update blocker.js

--- blocker.js
+++ blocker.js
@@ -1,6 +1,13 @@
+var thisURL = "teak1.github.io/wop/blocker.js";
 function exec(){
-  var search = this.location.search;
+  var search = "";
+  var scr = document.getElementsByTagName("srcipt");
+  for(var i = 0;i<scr.length;i++){
+    if(scr[i].src.split("?").includes(thisSRC)){
+      search=scr[i].src;
+    }
+  }
   document.body.innerHTML = JSON.stringify(this.location,null,2);
-  document.body.innerHTML+="<br/>test";
+  document.body.innerHTML+="<br/>"+search;
 }
 setTimeout(exec,1000);
